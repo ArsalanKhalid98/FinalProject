304eed163e0b7741fe6a6ce65eb877791c274c0b
LarsenLZM <Eliassagmolarsen@gmail.com>
2017-12-03 14:57:10 +0100

    eyy

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-window.onscroll = function () {tiptopp()};
+// checks if the site is scrolled more than 100px from the top, if not the up button stays hidden
+window.onscroll = function () {
+	magic()
+};
 
-function tiptopp() {
+function magic() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.getElementById("toppen").style.display = "block";
     } else {
         document.getElementById("toppen").style.display = "none";
     }
 }
-
-function topFunction() {
+// when you click the up button it triggers the function which then takes you to the top of the page
+function tiptopp() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+// deletes what ever is in the email slot, gives the costumer the idea that something has happend and the site works
+function slett() {
+	document.getElementById("email").value = "";
 }
