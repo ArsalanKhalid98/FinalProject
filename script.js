f066ab78ac78977a52df900d54d107df6773f747
larsenlzm <Eliassagmolarsen@gmail.com>
2018-11-04 16:44:12 +0100

    navbarv1

--- script.js
+++ script.js
@@ -35,3 +35,8 @@ function navpopoff(){
         x.className = "knapper";
     }
 }
+
+function closemenu() {
+    var x = document.getElementById("knapper");
+    if(x.className === "dropdown")x.className = "knapper";
+}
